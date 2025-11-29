944d090cf2101d7751708a0288ce74f5e7d67ca7
Dziakula <ulyanadziakovich@gmail.com>
2025-11-29 09:27:55 +0100

    Switch to PostgreSQL with dj_database_url

--- piorka_backend/settings.py
+++ piorka_backend/settings.py
@@ -86,10 +86,11 @@ WSGI_APPLICATION = 'piorka_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 
 
